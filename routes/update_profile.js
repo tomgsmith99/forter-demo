268bf6495f5ae5ac299377d99593b546cfb6eb4b
Tom Smith <tom.smith@forter.com>
2022-04-07 16:49:13 -0400

    remove duplicate get_factors func

--- routes/update_profile.js
+++ routes/update_profile.js
@@ -86,29 +86,3 @@ module.exports = function(app){
 		})
 	})
 }
-
-// function get_factors(user_id, callback) {
-
-// 	const url = process.env.BASE_URL
-// 	const apikey = process.env.OKTA_API_KEY
-
-// 	const config = {
-// 	  	method: 'get',
-// 	  	url: `${url}/api/v1/users/${user_id}/factors`,
-// 		headers: { 
-// 			'Accept': 'application/json', 
-// 			'Content-Type': 'application/json', 
-// 			'Authorization': `SSWS ${apikey}`
-// 		}
-// 	}
-
-// 	axios(config)
-// 	.then(function (response) {
-// 	  console.log(JSON.stringify(response.data))
-
-// 	  return callback(null, response.data)
-// 	})
-// 	.catch(function (error) {
-// 	  console.log(error)
-// 	})
-// }
